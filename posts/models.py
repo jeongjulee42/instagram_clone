76d0015e6a647c489d9b1f324e58ac7e1052a982
jeongjulee <ljj3342@naver.com>
2022-10-06 11:49:59 +0900

    Feat(Post): add post models
    post models
    host - who create post
    description - more information
    country, city - post location
    is_private - if is_private ture, your post show only to your friends

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
 
 
-class Post(models.Model):
-    pass
+class Post(core_models.TimeStampedModel):
+    
+    host = models.ForeignKey("users.User", related_name="posts", on_delete=models.CASCADE)
+    description = models.TextField(null=True)
+    country = CountryField(null=True, blank=True)
+    city = models.CharField(max_length=80, null=True, blank=True)
+    is_private = models.BooleanField(dafault=False)
