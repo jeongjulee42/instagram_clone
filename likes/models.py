268b2e17e6d2a824bea99b20e6bb57b6058c8e7d
jeongjulee <ljj3342@naver.com>
2022-10-09 20:37:12 +0900

    Feat(likes): add Like models

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,12 @@
+from email.policy import default
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Like(core_models.TimeStampedModel):
+    
+    """Like Model definition"""
+    
+    count = models.IntegerField(default=0)
+    user = models.ForeignKey("users.User", related_name="likes", on_delete=models.CASCADE)
+    post = models.ForeignKey("posts.Post", related_name="likes", on_delete=models.CASCADE)
