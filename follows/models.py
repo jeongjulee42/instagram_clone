2ed525a559f804f4ecb09e023002e099cf6d7912
jeongjulee <ljj3342@naver.com>
2022-10-09 20:49:16 +0900

    Feat(follow, follower): add models

--- follows/models.py
+++ follows/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Follow(core_models.TimeStampedModel):
+    
+    """Follow models definition"""
+    
+    user = models.ForeignKey("users.User", related_name="follow", on_delete=models.CASCADE)
+    follow_user = models.ManyToManyField("users.User", related_name="follow", blank=True, null=True)
