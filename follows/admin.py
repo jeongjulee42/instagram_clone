cb86a8671cd0547a4744dea202cd1bc610f23b6c
jeongjulee <ljj3342@naver.com>
2022-10-11 17:39:37 +0900

    Feat: add admin pannel

--- follows/admin.py
+++ follows/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Follow)
+class FollowAdmin(admin.ModelAdmin):
+    
+    """Follow admin pannel"""
+    
+    pass
+
+
+@admin.register(models.Follower)
+class FollowerAdmin(admin.ModelAdmin):
+    
+    """Follower admin pannel"""
+    
+    pass
--- likes/admin.py
+++ likes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Like)
+class LikeAdmin(admin.ModelAdmin):
+    
+    """Like admin pannel"""
+    
+    pass
