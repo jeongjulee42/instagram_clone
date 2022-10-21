9db687775a0a77d103311726bfeaa1be544f638b
jeongjulee <ljj3342@naver.com>
2022-10-21 22:08:22 +0900

    Feat: add likes models

--- likes/models.py
+++ likes/models.py
@@ -1,4 +1,3 @@
-from email.policy import default
 from django.db import models
 from core import models as core_models
 
@@ -6,6 +5,7 @@ from core import models as core_models
 class Like(core_models.TimeStampedModel):
     
     """Like Model definition"""
-    
+
     post = models.ForeignKey("posts.Post", related_name="likes", on_delete=models.CASCADE)
     userList = models.ManyToManyField("users.User", related_name="userList", blank=True)
+    
